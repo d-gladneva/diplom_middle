07683cf5bd5c4b9c22fbf25ff907e643cdd0595d
Dasha <davydova_di@mail.ru>
2020-10-23 16:06:01 +0300

    feat: open burger and close on menu

--- src/modules/burgerMenu.js
+++ src/modules/burgerMenu.js
@@ -1,19 +1,46 @@
 const burger = () => {
     const menuButton = document.querySelector('.menu-button');
+    const closeMenuLi = document.querySelector('.scroll');
+    console.log(closeMenuLi);
+    const closeMenuHref = closeMenuLi.querySelector('a');
+    console.log(closeMenuHref);
     const menuBtn = menuButton.querySelector('img');
     const closeMenuButton = document.querySelector('.close-menu-btn');
     const closeMenuBtn = closeMenuButton.querySelector('img');
     const openBurger = document.querySelector('.popup-menu');
+    let f = false;
+    document.addEventListener('click', (e) => {
+        if (document.documentElement.clientWidth < 768) {
+            let target = e.target;
+            console.log(target);
+            if (!f) {
+                target = target.closest('.menu-button');
+                if (target) {
+                    openBurger.style.display = 'block';
+                    f = true;
+                }
+            } else if (f) {
+                if (target === closeMenuBtn) {
+                    openBurger.style.display = 'none';
+                    f = false;
+                } else {
+                    if (target.classList.contains('test')) {
+                        openBurger.style.display = 'none';
+                        f = false;
+                    }
+                }
+            }
+        }
+    });
 
-    if (document.documentElement.clientWidth < 768) {
-        menuBtn.addEventListener('click', () => {
-            openBurger.style.display = 'block';
-        });
-        closeMenuBtn.addEventListener('click', () => {
-            openBurger.style.display = 'none';
-        });
 
-    }
+    // menuBtn.addEventListener('click', () => {
+    //     openBurger.style.display = 'block';
+    // });
+    // closeMenuBtn.addEventListener('click', () => {
+    //     openBurger.style.display = 'none';
+    // });
+
 };
 
 export default burger;
